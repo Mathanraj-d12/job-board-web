4706784a1cf30c81d29ef939d3586d3433821d50
Mathanraj-d12 <mathanrajd12@gmail.com>
2025-05-03 07:11:07 -0500

    Updates

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -16,8 +16,8 @@ messaging.onBackgroundMessage((payload) => {
   const notificationTitle = payload.notification.title;
   const notificationOptions = {
     body: payload.notification.body,
-    icon: '/vite.svg',
-    badge: '/vite.svg'
+    icon: '/job-board-web/vite.svg',
+    badge: '/job-board-web/vite.svg'
   };
 
   self.registration.showNotification(notificationTitle, notificationOptions);
